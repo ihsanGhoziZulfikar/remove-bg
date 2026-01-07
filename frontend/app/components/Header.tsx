99b619528ce6d6c71fc4f3ab0fa1cea459e54834
Rekabriyan <rekabriyanch08@gmail.com>
2026-01-07 10:41:21 +0700

    fix: update Header component to wrap logo in Link or mproved navigationfi

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className="bg-white shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
-        <div className="flex items-center gap-2">
+        <Link href='/' className="flex items-center gap-2">
           <img
             src="/asset/Frame 3.png"
             alt="logo"
             className="h-6 sm:h-8 lg:h-10 w-auto" />
-        </div>
+        </Link>
         <select className="text-blue-400 rounded-lg px-2 sm:px-3 py-1 sm:py-1.5 text-xs sm:text-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
           <option>English</option>
           <option>Indonesia</option>
